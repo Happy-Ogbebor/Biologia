02bfcceae21cedadd174d80b3c679168079c93d4
Happy-Ogbebor <149865512+Happy-Ogbebor@users.noreply.github.com>
2023-11-18 09:37:54 +0100

    Add files via upload

---
+++ Ricerca.js
@@ -0,0 +1,17 @@
+var articolo = ('o','i','u','k','l','j','gf','sd','yu','q');
+
+//console.log(articolo);
+var ricerca = " ";
+var trovato = false;
+var pos = -1;
+    
+console.log("Inserire l'elemento da cercare: ");
+//ricerca = Console.Log();
+for(var i = 0; i < 10 && !trovato; i++)
+{
+    if(ricerca == articolo)
+    {
+        trovato = true;
+        pos = 1;
+    }
+}
